72b8cf1d1200d146fae7afa0150446d686360467
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2023-09-03 09:21:24 +0600

    config env file in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -22,6 +22,8 @@ import store from './redux/store.js'
 import './index.scss'
 import axios from 'axios'
 
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000/api"
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />} errorElement={<ErrorPage />}>
@@ -34,12 +36,10 @@ const router = createBrowserRouter(
   )
 )
 
-axios.defaults.baseURL = "http://localhost:3000/api"
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
